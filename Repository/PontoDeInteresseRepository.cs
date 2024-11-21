42ed190ef12fec8edcc6b44b92cedeab3b45055e
MarcoAurelioArJes <marcoaureliodea.jesus@gmail.com>
2024-11-21 03:19:56 -0300

    feat: conectando no banco e criando seed de pontos de interesse

---
+++ Repository/PontoDeInteresseRepository.cs
@@ -0,0 +1,35 @@
+﻿using SQLite;
+using PrototipoMapeamentoAPP.Model;
+using PrototipoMapeamentoAPP.Repository.Seeds;
+
+namespace PrototipoMapeamentoAPP.Repository
+{
+    public class PontoDeInteresseRepository
+    {
+        private readonly MapeamentoDatabase _mapeamentoDatabase;
+        private readonly SQLiteAsyncConnection _database;
+        public PontoDeInteresseRepository()
+        {
+            _mapeamentoDatabase = new MapeamentoDatabase();
+            _database = _mapeamentoDatabase.Database;
+        }
+
+        public void Seed()
+        {
+            _database.InsertAllAsync(PontosDeInteresseSeed.PontosDeInteresses);
+        }
+
+        public void Criar(PontoDeInteresse pontoDeInteresse)
+        {
+            _database.InsertAsync(pontoDeInteresse);
+        }
+
+        public async Task<List<PontoDeInteresse>> ObterPorFiltro(string texto)
+        {
+            texto = texto.Trim().ToLower();
+            return await _database.Table<PontoDeInteresse>()
+                            .Where(c => texto == string.Empty || c.Nome.ToLower().Contains(texto))
+                            .ToListAsync();
+        }
+    }
+}
